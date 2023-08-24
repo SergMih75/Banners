736f6e3e6972bd0df8d96cf1cff4964b0bcbc8b8
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2023-08-25 02:34:02 +0300

    paralax js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,100 @@
-"use strict"
+"use strict";
 
-function paralax(event) {
-    // console.log(event);
-// let bgField = 0
-    this.querySelectorAll ('.bg').forEach(bg => {
-        // console.log('bg: ', bg);
-        console.log(event.clientX);
+function paralaxHeader(elem) {
+    elem.addEventListener("mousemove", () => {
 
-    // bg.style.transform = 'translateX'+ event.clientX +'px'
-    bg.style.transform = "translate(",+ event.clientX +"px)"
-    bg.style.transformransform = `translateX (${event.clientX}px)`
+    let rect = elem.getBoundingClientRect();
+
+    console.log(event.clientX - rect.left);
+
+    if ((event.clientX - rect.left -164)<0) {
+    elem.children[0].style.left = event.clientX - rect.left -164 + "px";
+    }
     });
-    // console.log(this.querySelectorAll ('.banners__bg-fild'));
+
+    elem.addEventListener('mouseleave', () => {
+        if(window.screen.width > 392) {
+        elem.children[0].style.left = -164 + "px";
+        } 
+        if (window.screen.width < 392){
+            elem.children[0].style.left = -0.5 + "px";
+        }
+    })
 }
 
-document.addEventListener('mousemove',paralax)
+function paralaxSide(elem) {
+    elem.addEventListener("mousemove", () => {
+
+    let rect = elem.getBoundingClientRect();
+
+    if ((event.clientX - rect.left -26)<-14) {
+    elem.children[0].style.left = event.clientX - rect.left -26 + "px";
+    }
+    if ((event.clientY - rect.top -30)<-21) {
+    elem.children[0].style.top = event.clientY - rect.top -30 + "px";
+    }
+    });
+
+    elem.addEventListener('mouseleave', () => {
+        elem.children[0].style.left = -26 + "px";
+        elem.children[0].style.top = -30 + "px";
+    })
+}
+
+function paralaxMap(elem) {
+    elem.addEventListener("mousemove", () => {
+
+    let rect = elem.getBoundingClientRect();
+
+    
+    if (window.screen.width > 392) {
+        if ((event.clientX - rect.left -206.7)<-199.7) {
+    elem.children[0].style.left = event.clientX - rect.left -100.7 + "px";
+        }
+        if ((event.clientY - rect.top -27)<1) {
+    elem.children[0].style.top = event.clientY - rect.top -27 + "px";
+        }
+    }else{
+        if ((event.clientX - rect.left -45)<-199.7) {
+    elem.children[0].style.left = -45 + "px";
+}
+if ((event.clientY - rect.top -14)<4) {
+    elem.children[0].style.top = -14 + "px";
+}
+    }
+    });
+
+    elem.addEventListener('mouseleave', () => {
+        if (window.screen.width > 392) {
+        elem.children[0].style.left = -206.7 + "px";
+        elem.children[0].style.top = -27 + "px";
+        }else{
+            elem.children[0].style.left = -45 + "px";
+            elem.children[0].style.top = -14 + "px";
+        }
+    })
+}
+
+function cursorOver() {
+    document.addEventListener("mouseover", (elem) => {
+        
+    if (
+    elem.target.classList.contains("banner-header__bg") &&
+    (window.screen.width > 392)
+    ) {
+    paralaxHeader(elem.target);
+    }
+
+    if (elem.target.classList.contains("banner-side__bg")) {
+    paralaxSide(elem.target);
+    }
+
+    if (elem.target.classList.contains("banner-map__bg")) {
+    paralaxMap(elem.target);
+    }
+    });
+}
+
+if (window.screen.width > 392) {
+    cursorOver();
+}
