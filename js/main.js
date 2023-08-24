3c83a0748c8ea7f840fc1ef0340942d6e5e84ddd
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2023-08-24 17:17:37 +0300

    +

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 "use strict"
+
+function paralax(event) {
+    // console.log(event);
+// let bgField = 0
+    this.querySelectorAll ('.bg').forEach(bg => {
+        // console.log('bg: ', bg);
+        console.log(event.clientX);
+
+    // bg.style.transform = 'translateX'+ event.clientX +'px'
+    bg.style.transform = "translate(",+ event.clientX +"px)"
+    bg.style.transformransform = `translateX (${event.clientX}px)`
+    });
+    // console.log(this.querySelectorAll ('.banners__bg-fild'));
+}
+
+document.addEventListener('mousemove',paralax)
